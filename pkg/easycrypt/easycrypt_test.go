aa1d87bd0b12f7ced791f72bc9b218b2523ac9e0
direktoren <urug98@gmail.com>
2020-10-11 22:57:03 +0200

    Format of easycrypt_test.go

--- pkg/easycrypt/easycrypt_test.go
+++ pkg/easycrypt/easycrypt_test.go
@@ -10,12 +10,9 @@ func TestEncryptDecrypt(t *testing.T) {
 	originalBytes := []byte("This is the test string we are encrypting/decrypting")
 	key := "ThisIsMy32BytesKeyForTestingFine"
 	encryptedBytes, err := Encrypt(originalBytes, key)
-
 	assert.Equal(t, err, nil, "Failed to Encrypt")
 
 	copyOfBytes, err := Decrypt(encryptedBytes, key)
-
 	assert.Equal(t, err, nil, "Failed to Decrypt")
-
 	assert.Equal(t, originalBytes, copyOfBytes, "Encrypt / Decrypt corrupted the testStr")
 }
